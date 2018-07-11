8743dd06ba8bd476b758ccfbd0fdd186f6e7150e
Nils Kuhn <nils.kuhn@iteratec.de>
2018-07-11 12:26:24 +0200

    add possibility to use a symmetrically encrypted secret key

--- README.md
+++ README.md
@@ -10,8 +10,11 @@ To read/decrypt a password from the store one can use the image/container like a
             -v </path/to/local/.password-store>:/root/.password-store \
             -v </path/to/private/gpg_key.asc>:/tmp/key.asc \
             -e PASSPHRASE=<passphrase_of_key> \
+            -e KEY_ENCRYPTED=<true|false> \
             iteratec/passwordstore <name_of_password_to_decrypt>
 
 The gpg private key can be exported via
 
         gpg2 --export-secret-key -o gpg_key.asc <ID_of_your_key>
+
+The container decrypts a symmetrically encrypted key before it uses it if one sets env variable `KEY_ENCRYPTED=true`.
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # This entrypoint 
+#   * gpg key is decrypted using passphrase given as env var $PASSPHRASE if env var $KEY_ENCRYPTED is set to true. 
 #   * Adds gpg private key which have to be mounted into the container.
-#   * Adds passphrase of the key to gpg-agent. Passphrase has to be mounted
-#     into the container, too.
+#   * Adds passphrase of the key to gpg-agent.
 #   * Calls gopath with given password entry name (must be given by the user as docker CMD) to show 
 #     entries password.
 
-gpg --import --passphrase $PASSPHRASE /tmp/key.asc &> /dev/null
+KEY_TO_USE="/tmp/key.asc"
+if [ "$KEY_ENCRYPTED" = true ]; then
+    echo $PASSPHRASE | gpg -o /tmp/key_decrypted.asc --passphrase-fd 0 /tmp/key.asc &> /dev/null
+    KEY_TO_USE="/tmp/key_decrypted.asc"
+fi
+gpg --import --passphrase $PASSPHRASE $KEY_TO_USE &> /dev/null
 for Keygrip in $(gpg --with-keygrip -K | awk -F"[ =]+" '/Keygrip/{print $3}'); do 
     /usr/lib/gnupg2/gpg-preset-passphrase -c --passphrase $PASSPHRASE $Keygrip; 
 done
@@ -15,6 +20,7 @@ done
 DOCKER_CMD=$1
 if [ -z "$DOCKER_CMD" ]; then
     echo "Usage: docker run ... iteratec/passwordstore <name-of-passwordstore-entry>"
+    exit 1
 else
     gopass show --password $DOCKER_CMD
 fi
